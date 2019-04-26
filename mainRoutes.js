e362e7c86284016d94ccb655856cd5c2ce9e6f78
ThabisoMagwaza <836403@students.wits.ac.za>
2019-04-26 17:31:54 +0200

    index route fixed

--- mainRoutes.js
+++ mainRoutes.js
@@ -5,7 +5,7 @@ let path = require('path')
 
 router.get('/', (req, res) => {
   // res.send('listening from main router')
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 module.exports = router
